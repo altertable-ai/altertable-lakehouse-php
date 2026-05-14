7efa52a0f3c1e4e5f0730f988666f6ab9af8efc9
Albert <albert20260301@gmail.com>
2026-05-14 13:20:29 +0200

    ci: initial setup

---
+++ tests/BootstrapTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Altertable\Lakehouse\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class BootstrapTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        self::assertTrue(true);
+    }
+}
